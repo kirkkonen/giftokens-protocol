73d77a0f4ee89fabab98c756ed51381a2c711be8
Andrei Chepelev <kirkkonen@gmail.com>
2023-05-02 10:14:20 +0300

    added all cases to the test script

--- test/gifttokenV5.test.ts
+++ test/gifttokenV5.test.ts
@@ -51,17 +51,7 @@ describe("Giftokens", function () {
 
   it('mints, accepts payment and returns contribution', async function () {
     const { ben, gifter, org, gifttoken } = await loadFixture(deployGiftoken);
-    // const { erc20, erc20owner } = await loadFixture(deployErc20)
-
-    // mint NFT
-    await gifttoken.connect(org).mint(ben.address, 1000, 'http://test.com');
-
-    // add native coins
-    // await gifttoken.connect(gifter).acceptPayment(1000, hre.ethers.constants.AddressZero, 0, {value: 1000});
-
-    // add erc20 coins
-    // await erc20.connect(erc20owner).approve(gifttoken.address, 10)
-    // await gifttoken.connect(erc20owner).acceptPayment(1000, erc20.address, 10);
+    const { erc20, erc20owner } = await loadFixture(deployErc20)
 
     const myAddress = '0x90C10F9abb753cA860F3BF3D67C9b8d23deB9044'
     const uniAddress = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'
@@ -75,32 +65,57 @@ describe("Giftokens", function () {
     const uniContractFactory = await ethers.getContractFactory('ERC20')
     const uniContract = uniContractFactory.attach(uniAddress);
 
-    const contribution = ethers.utils.parseEther('1')
-    await uniContract.connect(me).approve(gifttoken.address, contribution);
+    const uniContribution = ethers.utils.parseEther('1')
+    const newErc20contribution = 10
 
-    await expect(gifttoken.connect(me).acceptPayment(1000, uniAddress, contribution)).changeTokenBalance(
-      uniContract, me, contribution.mul(-1),
+    // mint NFT
+    await gifttoken.connect(org).mint(ben.address, 1000, 'http://test.com');
+
+    // add native coins
+    await gifttoken.connect(gifter).acceptPayment(1000, hre.ethers.constants.AddressZero, 0, {value: 1000});
+
+    // add new erc20 coins
+    await erc20.connect(erc20owner).approve(gifttoken.address, newErc20contribution)
+    await gifttoken.connect(erc20owner).acceptPayment(1000, erc20.address, newErc20contribution);
+
+    // approve and add real erc20
+    await uniContract.connect(me).approve(gifttoken.address, uniContribution);
+
+    // @ts-ignore
+    await expect(gifttoken.connect(me).acceptPayment(1000, uniAddress, uniContribution)).changeTokenBalance(
+      uniContract, me, uniContribution.mul(-1),
     )
 
-    // show contributions
-    console.log('contributions after native claimed', await gifttoken.getContributions(1000));
+    // show contributions after adding
+    console.log('contributions after everything is added', await gifttoken.getContributions(1000));
 
-    // claim erc20 coins
+    // claim native
+    // @ts-ignore
+    await gifttoken.connect(ben).claimFunds(1000, hre.ethers.constants.AddressZero, gifter.address)    
+
+    // claim new erc20
+    // @ts-ignore
+    await expect(gifttoken.connect(ben).claimFunds(1000, erc20.address, erc20owner.address)).changeTokenBalance(
+      erc20, ben, newErc20contribution
+    )
+
+    // claim real erc20 coins
+    // @ts-ignore
     await expect(gifttoken.connect(ben).claimFunds(1000, uniAddress, me.address)).changeTokenBalance(
-      uniContract, ben, contribution,
+      uniContract, ben, uniContribution
     )
 
-    // show contributions
-    // console.log('contributions after erc20 claimed', await gifttoken.getContributions(1000));
+    // show contributions after everything is claimed
+    console.log('contributions after everything is claimed', await gifttoken.getContributions(1000));
 
     // //claim nft
-    // await gifttoken.connect(ben).claimNFT(1000);
+    await gifttoken.connect(ben).claimNFT(1000);
 
     // //nft owner
-    // console.log('nft owner: ', await gifttoken.connect(ben).ownerOf(1000));
+    console.log('nft owner: ', await gifttoken.connect(ben).ownerOf(1000));
 
     // //balanceOf erc20
-    // console.log('erc20 balance: ', ethers.utils.formatEther(await uniContract.balanceOf(me.address)));
+    console.log('erc20 balance: ', ethers.utils.formatEther(await uniContract.balanceOf(me.address)));
 
   })
 
